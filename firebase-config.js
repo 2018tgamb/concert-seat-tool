d638786fc5a80217593df9a4c62c725778a8bd13
Jin Hung <sportbass805@gmail.com>
2025-08-24 17:47:18 +0800

    upload config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+            apiKey: "AIzaSyC-FTdkJp7CAh0NraSz4BCJqAKP14q515c", // 請替換成您自己的金鑰
+            authDomain: "concert-seat-tool.firebaseapp.com",
+            projectId: "concert-seat-tool",
+            storageBucket: "concert-seat-tool.firebasestorage.app",
+            messagingSenderId: "936796810945",
+            appId: "1:936796810945:web:225a97b87dad0401a86e40",
+            measurementId: "G-8X402NX102"
+        };
